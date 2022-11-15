fc47e4175f5bdf697054ea374f5c4bfa2c030748
aggelos thomos <angelogr56@gmail.com>
2022-11-15 21:02:52 +0200

    del useless dir

--- internetTechPJ1/tamplate/dark-light.js
+++
@@ -1,7 +0,0 @@
-var content = document.getElementsByTagName("body")[0];
-    var darkMode = document.getElementById("dark-change");
-    darkMode.addEventListener('click', function(){
-        darkMode.classList.toggle('active');//allazi to laybel stin css etsi wste na litiourgisei to ekastote mode
-        content.classList.toggle('night');
-    })
-    
